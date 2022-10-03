356829e5645df9c13394b5a4a90477566737b9a7
Bensley7 <mde.benslimane@gmail.com>
2022-10-03 20:38:37 +0200

    add predict wrapper

---
+++ predict.py
@@ -0,0 +1,25 @@
+import argparse
+
+from predict.mpn_ffc_predict import pred as predmpn
+from utils.io import read_cfg
+
+def parse_opt():
+    parser = argparse.ArgumentParser(description=".")
+    parser.add_argument('--model_path', type=str, required = True, help='weight path of the model')
+    parser.add_argument("--smile", help="smile of a molecule", type=str, required = True)
+    parser.add_argument("--config_file", default="", help="path to config file", type=str)
+    parser.add_argument("opts", help="Modify config options using the command-line", default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    return args
+
+def pred(opt):
+    #Read config file
+    cfg = read_cfg(opt.config_file, opt.opts)
+    if cfg.MODEL.name == "mpn_ffd":
+        predmpn(**vars(opt))
+
+if __name__ == "__main__":
+    args = parse_opt()
+    pred(args)
--- predict/mpn_ffc_predict.py
+++ predict/mpn_ffc_predict.py
@@ -1,5 +1,6 @@
 from typing import List
 import sys
+import argparse
 
 import torch
 from tqdm import tqdm
@@ -9,7 +10,8 @@ sys.path.append("../")
 from dataset.loader import MoleculeDataLoader, MoleculeDataset
 from models.model import MoleculeModel
 from models.utils import activate_dropout
-
+from utils.utils import get_data_from_smile
+from utils.io import read_cfg, load_checkpoint
 
 def predict(
     model: MoleculeModel,
@@ -53,3 +55,52 @@ def predict(
         preds.extend(batch_preds)
 
     return preds
+
+def predict_from_model(model_path: str, smile: str, config_file: str, opts):
+    #Read config file
+    cfg = read_cfg(config_file, opts)
+    
+    print('Loading data')
+    pred_data = get_data_from_smile(smile, cfg)
+    
+    pred_data.reset()
+
+    if "cuda" in cfg.MODEL.device:
+        print('Moving model to cuda')
+    device = torch.device(cfg.MODEL.device)
+    #Load Data
+    model = load_checkpoint(model_path, device=device)
+    model.eval()
+    
+    dataset_type = "classification"
+    if len(cfg.DATA_DIGEST.labels_name) > 1:
+        dataset_type = "multiclass"
+
+    pred_data_loader = MoleculeDataLoader(
+        dataset=pred_data,
+        batch_size=1,
+        num_workers=1,
+    )
+    out = predict(model, pred_data_loader)
+    print("Property of smile ", smile, "is ", out[0])
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser(description=".")
+    parser.add_argument('--model_path', type=str, required = True, help='weight path of the model')
+    parser.add_argument("--smile", help="smile of a molecule", type=str, required = True)
+    parser.add_argument("--config_file", default="", help="path to config file", type=str)
+    parser.add_argument("opts", help="Modify config options using the command-line", default=None,
+                        nargs=argparse.REMAINDER)
+
+    args = parser.parse_args()
+    return args
+
+
+def pred(opt):
+    predict_from_model(**vars(opt))
+
+if __name__ == "__main__":
+    args = parse_opt()
+    eval(args)
+
